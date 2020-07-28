fe119dd43c9e0717cd1a2c662fb4eebdefa165e8
Trevor Knotts <trevorknotts@berkeley.edu>
2020-07-28 16:26:12 +0000

    Added route, view, and method for part 2.

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
+  
+  get 'movies/:id/similar_movies', to: 'movies#same_director', as: 'similar_movies'
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end

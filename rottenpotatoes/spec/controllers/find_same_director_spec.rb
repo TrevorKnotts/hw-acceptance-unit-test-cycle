0504800427fe86a2dca60aee5edf340033345387
Trevor Knotts <trevorknotts@berkeley.edu>
2020-07-30 23:51:52 +0000

    Added some code to spec. Need to debug.

--- rottenpotatoes/spec/controllers/find_same_director_spec.rb
+++ rottenpotatoes/spec/controllers/find_same_director_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe MovieController do
   end
   describe "GET similar_movies" do
     it "receives click from 'Find With Same Director'" do
-      expect(same_director).to_receive(params[:id]).to_eq(@movie.id)
+      allow(Movie).to receive(:where).and_return(@movie)
+      get 'same_director', {:id => @movie.id}
+      expect(response).to render(same_director_path)
+    end
+  end
+  
+  describe "GET similar_movies" do
+    it "Checks for similar movies when there is no director" do
+      allow(Movie).to receive(:where).and_return([])
+      get 'same_director', {:id => @movie.id}
+      expect(response).to redirct_to(root_path)
     end
   end
 
